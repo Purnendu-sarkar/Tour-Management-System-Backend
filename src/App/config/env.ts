239ba64165a6f9b7064051cc941b190e0a63a5ca
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-07-25 14:17:23 +0600

    User / part 01 complete

--- src/App/config/env.ts
+++ src/App/config/env.ts
@@ -26,4 +26,4 @@ const loadEnvVariables = (): EnvConfig => {
     }
 }
 
-export const envVars = loadEnvVariables()                         
+export const envVars = loadEnvVariables()                       
