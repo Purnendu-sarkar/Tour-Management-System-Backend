beb77e40ac88af7e3089f75b2296f89b7ee24b64
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-07-25 19:50:12 +0600

    Auth / part 02 Complete

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,9 @@ app.use(cors())
 app.use("/api/v1", router)
 
 
+
+
+
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome to Tour Management System Backend"
